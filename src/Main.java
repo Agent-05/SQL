724f093be530d00d3dd3b0e8562f29ceac5bc07d
Agent-05 <falkwish15@gmail.com>
2024-03-27 13:40:33 -0500

    sql beginnings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.sql.*;
+class Main{
+    public static void main(String args[]){
+        addStudent("john", "dutton", 5);
     }
+
+    public static void addStudent(String firstName, String lastName, int id){
+        try{
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/school_manager","root","password");
+            Statement s =  con.createStatement();
+            s.execute("CREATE table student(id integer primary key not null, first_name text, last_name text);");
+            s.execute("INSERT INTO student (first_name, last_name) VALUES (\'firstName\', \'lastName\')");
+            con.close();
+        }catch(Exception e){ System.out.println(e);}
+    }
+
+
 }
